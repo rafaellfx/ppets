e119c0bf8c8afe6e814174ee011725355b5947fb
Rafael Lima Felix <rafaellfx@gmail.com>
2020-04-22 13:06:45 -0300

    add ignore

--- app/src/main/java/br/com/rafaellfx/ppets/adapter/PetAdapter.kt
+++ app/src/main/java/br/com/rafaellfx/ppets/adapter/PetAdapter.kt
@@ -2,6 +2,7 @@ package br.com.rafaellfx.ppets.adapter
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import br.com.rafaellfx.ppets.AboutPet
 import br.com.rafaellfx.ppets.R
 import br.com.rafaellfx.ppets.model.Pet
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.item_pet.view.*
 
 class PetAdapter(private val myDataset: List<Pet>, private val context: Context) :
@@ -29,6 +31,11 @@ class PetAdapter(private val myDataset: List<Pet>, private val context: Context)
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.viewRoot.txtName.text = "${myDataset[position].name}"
+
+        Glide.with(context)
+            .load("https://firebasestorage.googleapis.com/v0/b/ppets-71bfa.appspot.com/o/pluto.jpeg?alt=media&token=33306dbe-6010-4891-a33f-35d7e46309d5")
+            .into(holder.viewRoot.imgPet);
+
         holder.viewRoot.setOnClickListener {
             val intent = Intent(context, AboutPet::class.java)
             intent.putExtra("pet", myDataset[position])
